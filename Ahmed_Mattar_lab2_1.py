6c10107fd45ea4bd985314cec5b3621f15240a70
NEWVIRUS1 <danger11165@gmail.com>
2021-09-20 08:40:44 +0400

    added lab2

---
+++ Ahmed_Mattar_lab2_1.py
@@ -0,0 +1,13 @@
+import turtle
+
+def test_drive():
+    turtle.fd(100)
+    turtle.left(87)
+    turtle.setheading(90)
+    turtle.up()
+    turtle.goto(50,50)
+    turtle.down()
+    turtle.home()
+    
+    turtle.circle(25)
+test_drive()
